939a0d3300bc684cd296aedd43f9de98be686968
Kevin Englert <kevin.englert@gmail.com>
2014-08-17 16:58:40 -0400

    updated personal Site on Sinatra

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://www.rubygems.org'
 ruby '1.9.3'
 
 gem 'sinatra'
+gem 'activerecord'
+gem 'sinatra-activerecord'
+gem 'sqlite3'
+gem 'rake'
--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
+require 'sinatra/activerecord'
+
+require './models'
+
+set :database, "sqlite3:development.sqlite3"
+
 get '/' do 
 	erb :home
 end
